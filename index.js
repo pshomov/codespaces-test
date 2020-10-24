e7a684cb4abc5038b3512b486ebe537569386901
Petar Shomov <pshomov@gmail.com>
2020-10-24 09:18:33 +0000

    now trying a devcontainer

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log(`starting ...`)
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => { res.send('Hello') });
+
+app.listen(3000, () => {
+    console.log(`Now listening ...`)
+})
